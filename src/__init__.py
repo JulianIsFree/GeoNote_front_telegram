761d59eea47cd947560695e2f0107b7e758a86c8
Julian <bulat.zhantulikov@gmail.com>
2023-05-25 22:08:13 +0700

    image generation

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
+import base64
 import enum
 import sys
 import requests
 import argparse
 
+import telebot.apihelper
 from telebot import TeleBot
 from telebot.types import *
 from telebot import apihelper
@@ -43,7 +45,7 @@ class UserInfo:
 
 class Provider:
     def __init__(self, host: str):
-        assert(":" in host)
+        assert (":" in host)
         self.host = host
 
     @staticmethod
@@ -71,14 +73,37 @@ class Provider:
         self._post_url(url, params)
 
 
+class ImageProvider:
+    def __init__(self, host):
+        self.host = host
+
+    @staticmethod
+    def _post_url(url, body):
+        return requests.post(url, json=body)
+
+    def generate(self, text):
+        data = {
+            "prompt": str(text),
+            "width": 512,
+            "height": 512,
+            "steps": 60,
+            "cfg_scale": 5,
+            "seed": 0
+        }
+        print(str(text))
+        res = self._post_url(self.host + "/sdapi/v1/txt2img", data)
+        return res.json()['images'][0]
+
+
 class Bot:
-    def __init__(self, token, server_api_provider):
+    def __init__(self, token, server_host, image_host):
         # required for middleware, must be called before init of TeleBot
         apihelper.ENABLE_MIDDLEWARE = True
         self.bot = TeleBot(token)
         self.user_info = dict()
 
-        self.provider = server_api_provider
+        self.provider = Provider(server_host)
+        self.image_provider = ImageProvider(image_host)
         self.setup()
 
     def setup(self):
@@ -94,7 +119,7 @@ class Bot:
                 self.user_info[user] = UserInfo(user, msg.chat.id, State.WAIT)
 
         @bot.message_handler(commands=['my_notes'])
-        def all_notes(msg: Message):
+        def all_notes_pages(msg: Message):
             user = msg.from_user.id
             self.user_info[user].set_notes_pages(self.provider.get_all_notes(user))
             send_note(user, msg.chat.id, 1)
@@ -114,7 +139,7 @@ class Bot:
 
         @bot.message_handler(commands=['start'], func=lambda msg: self.user_info[msg.from_user.id].state == State.WAIT)
         def on_start(msg: Message):
-            bot.reply_to(msg, "Welcome! Try /note first. You can use /my_notes at any moment")
+            bot.reply_to(msg, "Welcome! Try /note first. You can use /my_notes to load all your notes")
 
         @bot.message_handler(commands=['note'], func=lambda msg: self.user_info[msg.from_user.id].state == State.WAIT)
         def on_note(msg: Message):
@@ -122,7 +147,8 @@ class Bot:
             bot.reply_to(msg, "Type anything you want and after use /post or /cancel")
 
         @bot.message_handler(commands=['cancel'], func=lambda msg: self.user_info[msg.from_user.id].state == State.NOTE \
-                                                                   or self.user_info[msg.from_user.id].state == State.POST)
+                                                                   or self.user_info[
+                                                                       msg.from_user.id].state == State.POST)
         def on_cancel(msg: Message):
             self.user_info[msg.from_user.id].set_state(State.WAIT)
             bot.reply_to(msg, "Canceled, dropping messages")  # TODO: may be not drop, but let user write another msg
@@ -135,14 +161,15 @@ class Bot:
             markup = ReplyKeyboardMarkup(row_width=2)
             markup.add(KeyboardButton(text="/post_with_geo", request_location=True))
             markup.add(KeyboardButton(text="/post_without_geo"))
-            bot.reply_to(msg, text="Done", reply_markup=markup)
+            bot.reply_to(msg, text="Choose whatever send with geomark or without", reply_markup=markup)
 
         @bot.message_handler(func=lambda msg: self.user_info[msg.from_user.id].state == State.NOTE)
         def on_echo_note(msg: Message):
             self.user_info[msg.from_user.id].add_to_note(msg.text)
             bot.reply_to(msg, "Recorded")
 
-        @bot.message_handler(commands=['post_with_geo'], func=lambda msg: self.user_info[msg.from_user.id].state == State.POST)
+        @bot.message_handler(commands=['post_with_geo'],
+                             func=lambda msg: self.user_info[msg.from_user.id].state == State.POST)
         def on_post_with_geo(msg):
             (x, y) = msg.location
             user = msg.from_user.id
@@ -151,8 +178,10 @@ class Bot:
             self.provider.post_with_geo(user, text, x, y)
             self.user_info[user].set_state(State.WAIT)
             bot.reply_to(msg, "Done", reply_markup=ReplyKeyboardRemove())
+            self.send_picture(text, msg.chat.id)
 
-        @bot.message_handler(commands=['post_without_geo'], func=lambda msg: self.user_info[msg.from_user.id].state == State.POST)
+        @bot.message_handler(commands=['post_without_geo'],
+                             func=lambda msg: self.user_info[msg.from_user.id].state == State.POST)
         def on_post_without_geo(msg):
             user = msg.from_user.id
             text = self.user_info[user].note
@@ -160,19 +189,27 @@ class Bot:
             self.provider.post_without_geo(user, text)
             self.user_info[user].set_state(State.WAIT)
             bot.reply_to(msg, "Done", reply_markup=ReplyKeyboardRemove())
+            self.send_picture(text, msg.chat.id)
 
     def start(self):
         self.bot.infinity_polling()
 
+    def send_picture(self, prompt, chat_id):
+        img = self.image_provider.generate(prompt)
+        telebot.apihelper.send_photo(self.bot.token, chat_id, base64.decodebytes(bytes(img, 'utf-8')))
+
 
 def main(argv):
     parser = argparse.ArgumentParser(description="TeleBot for interaction with GeoNote. Work in progress.")
-    parser.add_argument("--host", help="server address in format like http://example.com or http://ip:port",
-                        default="http://localhost:8080")
+    parser.add_argument("--host-server", help="server address in format like http://example.com or http://ip:port",
+                        default="http://localhost:7861")
+    parser.add_argument("--host-image",
+                        help="picture server address in format like http://example.com or http://ip:port",
+                        default="http://localhost:7860")
     parser.add_argument("token", help="token for telegram bot api to authorise")
 
     args = parser.parse_args(argv[1::])
-    bot = Bot(args.token, Provider(args.host))
+    bot = Bot(args.token, args.host_server, args.host_image)
     bot.start()
 
 
